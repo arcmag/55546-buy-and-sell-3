5d5032f57fe4e672059632976c7a7b152d6865d1
Николай <arcmag-work@mail.ru>
2020-03-25 22:55:53 +0700

    Обновляет установку порта с помощью переменной окружения

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -6,8 +6,9 @@ const DEFAULT_PORT = 8080;
 
 module.exports = {
   name: `--server`,
-  run(port) {
-    require(`../../express`).listen(port || DEFAULT_PORT, () => {
+  run() {
+    const envPort = process.env.PORT;
+    require(`../../express`).listen(envPort ? envPort.trim() : DEFAULT_PORT, () => {
       logger.info(`Запуск сервера`);
     }).on(`error`, (err) => {
       logger.error(`Server can't start. Error: ${err}`);
